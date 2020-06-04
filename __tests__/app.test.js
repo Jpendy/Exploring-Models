54cbd7550382d58cb5941bb3c7d6fccf857ea6f9
Jpendy <Jpendy256@gmail.com>
2020-06-04 14:20:46 -0700

    added get and post routes

---
+++ __tests__/app.test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const request = require('supertest');
+const app = require('../lib/app');
+const Shareable = require('../lib/models/Shareable');
+
+describe('app routes', () => {
+  const mongo = new MongoMemoryServer();
+  beforeAll(() => {
+    return mongo.getUri()
+      .then(uri => mongoose.connect(uri, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }));
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+
+  
+});
